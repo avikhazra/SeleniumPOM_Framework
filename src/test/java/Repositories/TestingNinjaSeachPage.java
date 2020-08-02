a27be20a4245d68fb4cc0528cc7dd20732c05a29
Avik Hazra <hazraavik11@gmail.com>
2020-08-02 22:11:15 +0530

    update

--- src/test/java/Repositories/TestingNinjaSeachPage.java
+++ src/test/java/Repositories/TestingNinjaSeachPage.java
@@ -7,6 +7,6 @@ public class TestingNinjaSeachPage {
 	public static By Searchinproductdescriptions =By.xpath("(//label[@class='checkbox-inline'])[2]/input");
 	public static By  SearchPage_Seach=By.xpath("//input[@id='button-search']");
 	public static By ProductSelect=By.xpath("//img[@title='xxxx']/ancestor::a[1]/ancestor::div[1]/ancestor::div[1]/descendant::div[2]/descendant::div[2]/descendant::button[1]");
-	public static By ProductValue=By.xpath("//div[@id='cart']/button[1]/span");
+	public static By ProductValue=By.xpath("//div[@id='cart']/button[1]/span[1]/i[1]");
 	public static By CheckOutButton=By.xpath("//div[@id='cart']/button[1]");
 }
